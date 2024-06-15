97d73335eb0a4af26d9b6185f53ee1e5e2f274da
vlad <23aw0139@jec.ac.jp>
2024-06-15 12:14:51 +0900

    vlad update

--- chat-app/app/_layout.tsx
+++ chat-app/app/_layout.tsx
@@ -28,8 +28,6 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-    ヴラドです
-    追加情報
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
