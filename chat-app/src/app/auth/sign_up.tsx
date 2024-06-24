784cb4e06b1fbfa2e842977ef12197e2f843f8f8
vlad <23aw0139@jec.ac.jp>
2024-06-24 21:22:30 +0900

    add name when signing up

--- chat-app/src/app/auth/sign_up.tsx
+++ chat-app/src/app/auth/sign_up.tsx
@@ -3,15 +3,24 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'reac
 import Button from '../../components/Button'
 import { useState } from 'react'
 import { Link, router } from 'expo-router'
-import { auth } from '../../config'
+import { auth, db } from '../../config'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { setDoc, doc } from 'firebase/firestore'
 
-const handlePress = (email: string, password: string): void => {
-    console.log(email, password)
+const handlePress = (name: string, email: string, password: string): void => {
+    console.log(name, email, password)
     createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         console.log(userCredential.user.uid)
-        router.replace('/auth/log_in')
+
+        const data = {
+            _id: userCredential.user.uid,
+            fullName: name,
+            providerData: userCredential.user.providerData[0]
+        }
+        setDoc(doc(db, 'users', userCredential.user.uid), data).then(()=> {
+            router.replace('auth/log_in')
+        })
     })
     .catch((error) => {
         const { code, message } = error
@@ -22,11 +31,19 @@ const handlePress = (email: string, password: string): void => {
 
 const SignUp = (): JSX.Element => {
     const [ email, setEmail ] = useState('')
-    const [password, setPassword] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [ name, setName ] = useState('')
     return(
         <View style={styles.container}>
             <View style={styles.inner}>
                 <Text style={styles.title}>Sign Up</Text>
+                <TextInput 
+                    style={styles.input} 
+                    value ={name}
+                    onChangeText={(text) => { setName(text) }}
+                    autoCapitalize='none'
+                    placeholder='Name'
+                />
                 <TextInput 
                     style={styles.input} 
                     value ={email}
@@ -45,7 +62,7 @@ const SignUp = (): JSX.Element => {
                     placeholder = 'Password'
                     textContentType='password'
                 />
-                <Button label='Submit' onPress={() => {handlePress(email, password)}}/>
+                <Button label='Submit' onPress={() => {handlePress(name, email, password)}}/>
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Already registered?</Text>
                     <Link href='/auth/log_in' asChild replace>
